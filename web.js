30ea29b6a628475b9aff002299b46c15cdabb27a
m00kid <theomiedema@gmail.com>
2013-07-16 00:49:22 +0200

    added index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var ifile=fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(ifile);
 });
 
 var port = process.env.PORT || 5000;
